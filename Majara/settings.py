a8ec228a44e5c91170b086630c1a7280178ae261
amirHossein <amirhosein_1379@yahoo.com>
2019-08-21 01:35:43 +0430

    Models

--- Majara/settings.py
+++ Majara/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import locale
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'majara',
-        'PASSWORD': 'Ferdosi!23',
-        'USER': 'ferdosi',
+        'PASSWORD': '',
+        'USER': 'root',
         'PORT': 3306,
     },
 }
@@ -127,6 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 
 locale.setlocale(locale.LC_ALL, "fa_IR.UTF-8")
--- blog/admin.py
+++ blog/admin.py
@@ -1,26 +1,83 @@
 from django.contrib import admin
 from django.utils.html import format_html
-
 from .models import *
 
-# Register your models here.
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
 
     def pictureShow(self, obj):
-        if obj.image == None:
-            return "بدون عکس"
+        if obj.image is None:
+            return 'بدون عکس'
         else:
             return format_html("<img src='%s' height=50px>" % obj.image.url)
 
-    list_display_links = ("title", "pictureShow", "slug")
-    list_display = ("title", "pictureShow", "slug")
+    list_display_links = ('title', 'pictureShow', 'slug')
+    list_display = ('title', 'pictureShow', 'slug')
     search_fields = ['slug', 'title']
 
     pictureShow.allow_tags = True
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Story)
-admin.site.register(DialogStory)
-admin.site.register(Author)
-admin.site.register(TextStory)
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+
+    def pictureShow(self, obj):
+        if obj.image is None:
+            return 'بدون عکس'
+        else:
+            return format_html("<img src='%s' height=50px>" % obj.image.url)
+
+    list_display = ('title', 'pictureShow', 'slug')
+    list_display_links = ('title', 'pictureShow', 'slug')
+    search_fields = ['slug', 'title']
+
+    pictureShow.allow_tags = True
+
+
+@admin.register(DialogStory)
+class DialogStoryAdmin(admin.ModelAdmin):
+
+    def pictureShow(self, obj):
+        if obj.image is None:
+            return 'بدون عکس'
+        else:
+            return format_html("<img src='%s' height=50px>" % obj.image.url)
+
+    list_display = ('storyId', 'pictureShow', 'dialogTextType')
+    list_display_links = ('storyId', 'pictureShow', 'dialogTextType')
+    search_fields = ['storyId', 'dialogTextType']
+
+    pictureShow.allow_tags = True
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+
+    def pictureShow(self, obj):
+        if obj.image is None:
+            return 'بدون عکس'
+        else:
+            return format_html("<img src='%s' height=50px>" % obj.image.url)
+
+    list_display_links = ('FirstName', 'LastName', 'pictureShow')
+    list_display = ('FirstName', 'LastName', 'pictureShow')
+    search_fields = ['FirstName', 'LastName']
+
+    pictureShow.allow_tags = True
+
+
+@admin.register(TextStory)
+class TextStoryAdmin(admin.ModelAdmin):
+
+    def pictureShow(self, obj):
+        if obj.image is None:
+            return 'بدون عکس'
+        else:
+            return format_html("<img src='%s' height=50px>" % obj.image.url)
+
+    list_display_links = ('storyId', 'pictureShow',)
+    list_display = ('storyId', 'pictureShow',)
+    search_fields = ['storyId', 'pageNumber']
+
+    pictureShow.allow_tags = True
